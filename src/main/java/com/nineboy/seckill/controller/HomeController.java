74b23f2ba82c14f5f5835fa8d1a289b6a3f8f72b
SunJiubo <940755052@qq.com>
2019-05-24 11:21:16 +0800

    5.24druid完成集成

--- src/main/java/com/nineboy/seckill/controller/HomeController.java
+++ src/main/java/com/nineboy/seckill/controller/HomeController.java
@@ -6,6 +6,8 @@ import com.nineboy.seckill.service.ProductDetailService;
 import com.nineboy.seckill.service.ProductInfoService;
 import com.nineboy.seckill.vo.ProductInfoVO;
 import org.apache.ibatis.annotations.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,8 @@ import java.util.List;
 @Controller
 @RequestMapping(path = "/homepage")
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 
     @Autowired
     private ProductInfoService productInfoService;
